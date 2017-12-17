65052c56ce9d1b6bb1f9c8ecf12de764a6e58fc4
yuanfeng <yuanfeng20000@gmail.com>
2017-12-18 07:51:27 +0800

    init

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import receive, reply
 import msg_handle
 
 app = Flask(__name__)
-app.debug=True
+app.threaded=True
 
 
 @app.route("/", methods=['GET'])
@@ -20,7 +20,7 @@ def root():
 
 def index():
     if request.method == 'GET':
-        token = '***' # your token
+        token = '41161a014eb70f1d54ea782c421c60b9' # your token
         query = request.args  # GET 方法附上的参数
         signature = query.get('signature', '')
         timestamp = query.get('timestamp', '')
@@ -72,22 +72,22 @@ def download_file(filename):
         response = send_from_directory(directory, 'static/syslog配参考置V1.41.doc', as_attachment=True)
         response.headers['Content-Type'] = 'application/msword'
     elif filename == '4':
-        response = send_from_directory(directory, 'static/**平台申请工单操作手册-20170705.doc', as_attachment=True)
+        response = send_from_directory(directory, 'static/安全管控平台申请工单操作手册-20170705.doc', as_attachment=True)
         response.headers['Content-Type'] = 'application/msword'
     elif filename == '5':
-        response = send_from_directory(directory, 'static/**平台账号&VPN申请表-分公司（20170426模板）.docx', as_attachment=True)
+        response = send_from_directory(directory, 'static/管控平台账号&VPN申请表-分公司（20170426模板）.docx', as_attachment=True)
         response.headers['Content-Type'] = 'application/msword'
     elif filename == '6':
-        response = send_from_directory(directory, 'static/**平台账号&VPN申请表-省公司（20170426模板）.docx', as_attachment=True)
+        response = send_from_directory(directory, 'static/管控平台账号&VPN申请表-省公司（20170426模板）.docx', as_attachment=True)
         response.headers['Content-Type'] = 'application/msword'
     elif filename == '7':
-        response = send_from_directory(directory, 'static/**安全合规操作说明文档.doc', as_attachment=True)
+        response = send_from_directory(directory, 'static/湖南安全合规操作说明文档.doc', as_attachment=True)
         response.headers['Content-Type'] = 'application/msword'
     elif filename == '8':
-        response = send_from_directory(directory, 'static/***安全管控平台应用资源授权调研表-V2.xls', as_attachment=True)
+        response = send_from_directory(directory, 'static/湖南移动安全管控平台应用资源授权调研表-V2.xls', as_attachment=True)
         response.headers['Content-Type'] = 'application/x-xls'
     elif filename == '9':
-        response = send_from_directory(directory, 'static/**申请模板.xlsx', as_attachment=True)
+        response = send_from_directory(directory, 'static/金库申请模板.xlsx', as_attachment=True)
         response.headers['Content-Type'] = 'application/x-xls'
     elif filename == '10':
         response = send_from_directory(directory, 'static/模板：程序帐号登记.xlsx', as_attachment=True)
@@ -96,13 +96,15 @@ def download_file(filename):
         response = send_from_directory(directory, 'static/模板：应急终端登记.xlsx', as_attachment=True)
         response.headers['Content-Type'] = 'application/x-xls'
     elif filename == '12':
-        response = send_from_directory(directory, 'static/**模板.xlsx', as_attachment=True)
+        response = send_from_directory(directory, 'static/绕行备案模板.xlsx', as_attachment=True)
         response.headers['Content-Type'] = 'application/x-xls'
     elif filename == '13':
-        response = send_from_directory(directory, 'static/**平台申请表.xls', as_attachment=True)
+        response = send_from_directory(directory, 'static/应用资源接入接入管控平台申请表.xls', as_attachment=True)
         response.headers['Content-Type'] = 'application/x-xls'
     else:
         response = "No such file !"
     response.headers['Access-Control-Allow-Origin'] = '*'
     return response
 
+if __name__=='__main__':
+    app.run(host="0.0.0.0", port=80, threaded = True)
